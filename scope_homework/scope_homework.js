0d8668be774873465d1ddf466a25e68e1ca0e095
Steve Vance <steve.vance@hotmail.co.uk>
2020-01-06 17:44:16 +0000

    episode 2 complete

--- scope_homework/scope_homework.js
+++ scope_homework/scope_homework.js
@@ -14,4 +14,19 @@
 //
 // declareMurderer is calling scenario.murderer and in this case the output will be 'The murderer is Miss Scarlet'.
 
-EPISODE 2
+// EPISODE 2
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//
+// declareMurderer is calling the function changeMurderer and in this case the output will be 'The murderer is Mrs. Peacock'.
+
+// EPISODE 3
