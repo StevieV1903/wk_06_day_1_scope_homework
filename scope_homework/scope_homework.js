590836df2cd5d8627a5b0401ea26599e72b02887
Steve Vance <steve.vance@hotmail.co.uk>
2020-01-06 18:39:16 +0000

    episode 5 complete

--- scope_homework/scope_homework.js
+++ scope_homework/scope_homework.js
@@ -46,17 +46,36 @@
 // The firstVerdict is calling the declareMurderer function and the output will be Mrs. Peacock. The second verdict the murderer is Professor Plum.
 
 // EPISODE 4
-let suspectOne = 'Miss Scarlet';
-let suspectTwo = 'Professor Plum';
-let suspectThree = 'Mrs. Peacock';
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
 
-const declareAllSuspects = function() {
-  let suspectThree = 'Colonel Mustard';
-  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// My first instinct here is that suspect three would be Colonel Mustard. However this proved wrong and on running it, Mrs. Peacock is suspect three.
+
+// EPISODE 5
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
 }
 
-const suspects = declareAllSuspects();
-console.log(suspects);
-console.log(`Suspect three is ${suspectThree}.`);
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
 
-// My first instinct here is that suspect three would be Colonel Mustard.
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
